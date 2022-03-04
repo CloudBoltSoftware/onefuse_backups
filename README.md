75ba3515fa9b49373437d0b76fbb52f4cae342df
Mike Bombard <37379633+mbomb67@users.noreply.github.com>
2022-03-04 08:09:59 -0500

    Updated script samples to call OneFuse Python Module for backups

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # OneFuse Backup and Restore Scripts
-This repo contains sample scripts to back up OneFuse Policies to a git repo and potentially restore them to an instance. These are only intended as samples only, and should not be used in production environments. 
+This repo contains a sample script to back up OneFuse Policies to a git repo. These are only intended as samples only, and should not be used in production environments. 
+
+To learn more about OneFuse Backups and Policy Restores, the documentation can be found at:
+
+[Backup OneFuse Policies](https://docs.cloudbolt.io/articles/#!onefuse-latest/backup-onefuse-policies-with-onefuse-python-module)
+
+[Restore OneFuse Policies](https://docs.cloudbolt.io/articles/#!onefuse-latest/restore-onefuse-policies-with-onefuse-python-module)
+
 
 ## Setup/Pre-Requisites
-1. Copy the entire onefuse_backups repo to /var/opt/cloudbolt/proserv/xui/
-   on the OneFuse appliance. If the xui directory doesn't exist, you can create it. 
+1. Copy the policy_backups.py script to /var/opt/cloudbolt/proserv/onefuse_backups/
+   on the OneFuse appliance. If the onefuse_backups directory doesn't exist, you can create it. 
 2. Create a Connection Info for onefuse. This must be labelled as 'onefuse', 
-   and named 'onefuse'
+   and named 'onefuse'. 
     - To do this manually from shell plus ssh in to the OneFuse appliance and then: 
         ```
         python /opt/cloudbolt/manage.py shell_plus
@@ -35,68 +42,16 @@ The backup script will:
 2. Use git to synch policies to a git repo
 
 ### Policy Backup Setup:
-1. Update BACKUPS_PATH below to reflect the subdirectory under the folder where you would like backups to end up. This would allow for multiple OneFuse instances to backup to the same git repo. To only backup a single OneFuse instance, GIT_PATH can equal BACKUPS_PATH
-2. Update GIT_PATH below to reflect the directory where the repo was cloned to
-3. Update GIT_AUTHOR below to reflect the author information
+1. Update BACKUPS_PATH in the policy_backups.py script to reflect the subdirectory under the folder where you would like backups to end up. This would allow for multiple OneFuse instances to backup to the same git repo. To only backup a single OneFuse instance, GIT_PATH can equal BACKUPS_PATH
+2. Update GIT_PATH in the policy_backups.py script to reflect the directory where the repo was cloned to
+3. Update GIT_AUTHOR in the policy_backups.py script to reflect the author information
 
 ### Execution
-Execute bacvkups by:
-    ```
-    python /var/opt/cloudbolt/proserv/xui/onefuse_backups/policy_backups.py
-    ```
-    
-    
+Execute backups by:
+  
+    python /var/opt/cloudbolt/proserv/onefuse_backups/policy_backups.py
+
     
 ## OneFuse Policy Restore Script
-### Purpose
-The restore script will: 
-1. Use git to pull content from git repo to FILE_PATH set below
-2. Connect to OneFuse via REST and restore all OneFuse Policies in the FILE_PATH
-
-### Policy Restore Setup:
-1. Update the FILE_PATH variable in the policy_restore.py to reflect the directory in the cloned repo that you would like to restore from.
-
-### Execution
-Execute a restore by:
-    ```
-    python /var/opt/cloudbolt/proserv/xui/onefuse_backups/policy_restore.py
-    ```
-###Restore Note: 
-If you use the restore script to restore any credentials, the script does
-not store or restore passwords. If credentials are created for an instance, you
-will need to update the password for each created credential. If the credentials
-already existed, this script will not overwrite the existing password, but will
-update all other properties of a credential.
-
-
-
-## OneFuse Restore Specific Policies Script
-### Purpose
-1. Restore all files to OneFuse passed in as args to the script.
-2. This script can restore OneFuse JSON policies either from a file on the 
-   local system or directly from a git repo. 
-3. This script is not aware of policy dependencies. If you have dependencies
-   (eg. Naming Policy needs a Naming Sequence, or Endpoint needs Credentials)
-   You will also need to pass in the json for the dependency as an argument 
-   prior to the dependent policy.
-
-### Execution
-1. This script can be executed by:
-    ```
-    python /var/opt/cloudbolt/proserv/xui/onefuse_backups/policy_restore.py <arg1> <arg2> <arg3>
-    ```
-   
-### Examples
-1. Restore a single Naming Policy from file
-    ```
-    python /var/opt/cloudbolt/proserv/xui/onefuse_backups/policy_restore.py '/var/opt/cloudbolt/proserv/se-1f-demo-backups/se-1f-demo-1-3/namingPolicies/docker_port.json'
-    ```
-2. Restore Naming Policy where dependency of Naming Sequence doesn't already exist on the server. Note: the dependency is called first:
-    ```
-    python /var/opt/cloudbolt/proserv/xui/onefuse_backups/policy_restore.py '/var/opt/cloudbolt/proserv/se-1f-demo-backups/se-1f-demo-1-3/namingSequences/BASE10_port.json' '/var/opt/cloudbolt/proserv/se-1f-demo-backups/se-1f-demo-1-3/namingPolicies/docker_port.json'
-    ```
-3. Restore a namingPolicy from a git repo. Note: be sure to use the raw link here. If it is a private repo, you will also want to include your token in the url.     
-    ```
-    python /var/opt/cloudbolt/proserv/xui/onefuse_backups/policy_restore.py https://raw.githubusercontent.com/CloudBoltSoftware/se-1f-demo-backups/main/se-1f-demo-1-3/namingPolicies/test_backups.json
-    ```
- 
+Restores can now be completed by using the OneFuse Python Module. 
+https://docs.cloudbolt.io/articles/#!onefuse-latest/restore-onefuse-policies-with-onefuse-python-module
--- policy_backups.py
+++ policy_backups.py
@@ -2,12 +2,18 @@
 """
 OneFuse Policy Backup Script
 
-This script will: 
+This is a sample script to get you started syncing OneFuse backups with git.
+This is intended as a sample only, it may need to be updated to match your
+specific use case.
+
+This script will:
 1. Connect to OneFuse via REST and store all OneFuse Policies in JSON in a local directory
 2. Use git to synch policies to a git repo
 
 Pre-Requisites: 
-1. Create a Connection Info for onefuse. This must be labelled as 'onefuse', and named 'onefuse'
+1. Logged in to the OneFuse appliance as root, install the OneFuse Python Module:
+    https://docs.cloudbolt.io/articles/#!onefuse-latest/python-module-getting-started
+2. Create a Connection Info for onefuse. This must be labelled as 'onefuse', and named 'onefuse'
         - To do this manually from shell plus: 
         > python /opt/cloudbolt/manage.py shell_plus
         > ci = ConnectionInfo(
@@ -21,21 +27,21 @@ Pre-Requisites:
         > ci.save()
         > ci.labels.add('onefuse')
         > ci.save()
-2. Use Git to clone repo to somewhere under /var/opt/cloudbolt/proserv/
+3. Use Git to clone repo to somewhere under /var/opt/cloudbolt/proserv/
     > mkdir /var/opt/cloudbolt/proserv/<directory name here>
     > cd /var/opt/cloudbolt/proserv/<directory name here>
     > git clone https://<git username>:<git password>@github.com/<repo url>
-3. Update BACKUPS_PATH below to reflect the subdirectory under the folder where 
+4. Update BACKUPS_PATH below to reflect the subdirectory under the folder where
    you would like backups to end up. This would allow for multiple OneFuse 
    instances to backup to the same git repo. To only backup a single OneFuse
    instance, GIT_PATH can equal BACKUPS_PATH
-4. Update GIT_PATH below to reflect the directory where the repo was cloned to
-5. Update GIT_AUTHOR below to reflect the author information
+5. Update GIT_PATH below to reflect the directory where the repo was cloned to
+6. Update GIT_AUTHOR below to reflect the author information
 
 Use: 
-1. Copy the entire onefuse_backups directory to /var/opt/cloudbolt/proserv/xui/ on the OneFuse appliance
+1. Copy the policy_backups.py script to /var/opt/cloudbolt/proserv/onefuse_backups/ on the OneFuse appliance
 2. This script can be executed by:
-    > python /var/opt/cloudbolt/proserv/xui/onefuse_backups/policy_backups.py
+    > python /var/opt/cloudbolt/proserv/onefuse_backups/policy_backups.py
 
 This script can also be scheduled using cron if desired to have schedule policy backups/versioning
 """
@@ -49,102 +55,22 @@ if __name__ == '__main__':
     sys.path.append('/opt/cloudbolt')
     django.setup()
 
-import sys
-import json
-from api_wrapper import OneFuseConnector
-import os
-import errno
+
+from onefuse.cloudbolt_admin import CbOneFuseManager
+from onefuse.backups import BackupManager
 import subprocess
 
 
-
-BACKUPS_PATH = '/var/opt/cloudbolt/proserv/se-1f-demo-backups/se-1f-demo-1-3/'
+BACKUPS_PATH = '/var/opt/cloudbolt/proserv/se-1f-demo-backups/cb-mb-01/'
 GIT_PATH = '/var/opt/cloudbolt/proserv/se-1f-demo-backups/'
 GIT_AUTHOR = 'OneFuse Admin <onefuse@cloudbolt.io>' #format: 'First Last <email@domain.com>', there must be a space between Last and <
+CONN_INFO_NAME = "onefuse"
 
-def create_json_files(response,policy_type,onefuse):
-    try:
-        response.raise_for_status()
-    except:
-        try: 
-            detail = response.json()["detail"]
-        except: 
-            error_string = (f'Unknown error. JSON: {response.json()}, ')
-            error_string += (f'Error: {sys.exc_info()[0]}. {sys.exc_info()[1]}, '
-                            f'line: {sys.exc_info()[2].tb_lineno}')
-            raise Exception(error_string)
-        if detail == 'Not found.':
-            #This may happen when script is run against older versions. 
-            print(f"WARN: policy_type not found: {policy_type}")
-            return False
-        else: 
-            error_string = (f'Unknown error. JSON: {response.json()}, ')
-            error_string += (f'Error: {sys.exc_info()[0]}. {sys.exc_info()[1]}, '
-                            f'line: {sys.exc_info()[2].tb_lineno}')
-            raise Exception(error_string)
-    
-    response_json = response.json()
-    
-    for policy in response_json["_embedded"][policy_type]:
-        #print(f'policy: {policy}')
-        #print(f'Backing up {policy_type} policy: {policy["name"]}')
-        filename = f'{BACKUPS_PATH}{policy_type}/{policy["name"]}'
-        if policy_type == "endpoints":
-            if "credential" in policy["_links"]:
-                policy["_links"]["credential"]["title"] = get_credential_name(policy,onefuse)                
-        if not os.path.exists(os.path.dirname(filename)):
-            try:
-                os.makedirs(os.path.dirname(filename))
-            except OSError as exc: # Guard against race condition
-                if exc.errno != errno.EEXIST:
-                    raise
-        if "type" in policy:
-            file_name = f'{BACKUPS_PATH}{policy_type}/{policy["type"]}_{policy["name"]}.json'
-        elif "endpointType" in policy:
-            file_name = f'{BACKUPS_PATH}{policy_type}/{policy["endpointType"]}_{policy["name"]}.json'
-        else: 
-            file_name = f'{BACKUPS_PATH}{policy_type}/{policy["name"]}.json'
-        f = open(file_name,'w+')
-        f.write(json.dumps(policy, indent=4))
-        f.close()
-    
-    return key_exists(response_json["_links"], "next")
-
-def get_credential_name(policy,onefuse): 
-    href = policy["_links"]["credential"]["href"]
-    url = href.replace('/api/v3/onefuse','')
-    response = onefuse.get(url)
-    try:
-        response.raise_for_status()
-    except: 
-        err_msg = f'Link could not be found for href: {href}'
-        raise Exception(err_msg)
-    return response.json()["name"]
-
-def key_exists(dict, key):  
-    if key in dict.keys():
-        return True
-    else:
-        return False 
 
 def main():
-    policy_types = [
-        "moduleCredentials","endpoints","validators","namingSequences","namingPolicies","propertySets","ipamPolicies","dnsPolicies","microsoftADPolicies","ansibleTowerPolicies",
-        "scriptingPolicies","servicenowCMDBPolicies","vraPolicies"
-    ]
-
-    #Gather policies from OneFuse, store them under BACKUPS_PATH
-    with OneFuseConnector("onefuse") as onefuse:
-        for policy_type in policy_types:
-            print(f'Backing up policy_type: {policy_type}')
-            response = onefuse.get(f'/{policy_type}/')
-            next_exists = create_json_files(response,policy_type,onefuse)
-            while next_exists:
-                next_page = response.json()["_links"]["next"]["href"]
-                next_page = next_page.split("/?")[1]
-                response = onefuse.get(f'/{policy_type}/?{next_page}')
-                next_exists = create_json_files(response,policy_type,onefuse)
-
+    ofm = CbOneFuseManager(CONN_INFO_NAME)
+    backups = BackupManager(ofm)
+    backups.backup_policies(BACKUPS_PATH)
           
     #Use git to synch changes to repo
     GIT_FILE = f'{GIT_PATH}.git'
